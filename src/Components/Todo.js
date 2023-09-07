4e0ce922862591d79c3bf3c40c16ad0ca58e6e2e
Amal S <amalsunu5469@gmail.com>
2023-09-07 12:32:05 +0530

    todos can be added & icons added

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,21 +1,59 @@
-import React from 'react'
-import './Todo.css'
+import React from 'react';
+import './Todo.css';
+import { useState , useRef , useEffect } from 'react';
+// icons
+import { IoMdDoneAll } from "react-icons/io";
+import {FiEdit} from "react-icons/fi";
+import { MdDelete} from "react-icons/md";
 
 function Todo(){
+        const[todo, setTodo] = useState('') //read data
+        const[todos, setTodos] = useState([]) //storing or updating todos
+        //to read and output todos 
+        const addTodo = ()=>{
+            setTodos([...todos, todo])
+            console.log(todos)
+            // to clear search bar
+            setTodo('')
+            
+        }
+        const handleSubmit = (e)=>{
+            e.preventDefault()
+        }
+
+        // To make it focus .use hook ,direct access DOM
+        const inputRef = useRef('null')
+
+        //load application , first call function will be useEffect , then remaining -> USEEFFECT refer hooks tutorial
+        //dependency control to avoid loop, initial loading only focus!
+        useEffect(()=>{
+            inputRef.current.focus()
+        })
+
     return(
         <div className='container'>
-            <h2>Todo App</h2>
-            <form className='form-group'>
-                <input type="text" placeholder='Enter your Todo' className='form-control'></input>
-                <button>ADD</button>
+            <h2>TODO !</h2>
+            <form className='form-group' onSubmit={handleSubmit}>
+                <input type="text" value={todo} ref={inputRef} placeholder='Enter your Todo' className='form-control box'  onChange={(event)=>setTodo(event.target.value)}></input>
+                {/* button */}
+                <button onClick={addTodo} className='box'>ADD</button> 
             </form>
             <div className='list'>
                 <ul>
-                    <li className='form-control'>First</li>
-                    <li className='form-control'>Second</li>
-                    <li className='form-control'>Third</li>
+                    {todos.map((todo)=>(
+                            
+                            <li  className='list-icons'>{todo}
+                                    <span>
+                                        <IoMdDoneAll/>
+                                        <FiEdit />
+                                        <MdDelete />
+                                    </span>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
+            
         </div>
     )
 }
